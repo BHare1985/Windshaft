41e0a60f3977383adf431c063ddc2226d7e97686
Brian Hare <bhare1985@gmail.com>
2016-06-23 02:30:32 -0400

    Revert back to 0.12 node

--- test/support/test_helper.js
+++ test/support/test_helper.js
@@ -6,6 +6,6 @@ var path = require('path');
 var os = require('os');
 
 var oldDir = path.join(__dirname, '..', '..', 'config', 'mssql', 'mssql-'+os.platform()+'.input');
-var newDir = path.join(__dirname, '..', '..', 'node_modules', 'mapnik', 'lib', 'binding', 'node-v46-'+os.platform()+'-x64', 'mapnik', 'input', 'mssql-'+os.platform()+'.input');
+var newDir = path.join(__dirname, '..', '..', 'node_modules', 'mapnik', 'lib', 'binding', 'node-v14-'+os.platform()+'-x64', 'mapnik', 'input', 'mssql-'+os.platform()+'.input');
 
 fs.writeFileSync(newDir, fs.readFileSync(oldDir));
