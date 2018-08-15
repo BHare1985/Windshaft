175b480c9fe5e1177b204a883e7587cc70b4fd92
Brian Hare <bhare1985@gmail.com>
2018-08-15 00:15:40 -0400

    Update servername

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,4 +1,4 @@
 module.exports.name             = 'development';
 module.exports.dbtype			= "mssql";
-module.exports.datasource 		= {user: "sa", password: "sa", host: "C-256\\sqlexpress", port: 1433};
+module.exports.datasource 		= {user: "sa", password: "sa", host: "localhost", port: 1433};
 module.exports.windwalkerPort   = 8080;
